de39ab29c86e68adb987e2db5c2701e080200809
Andy Henson <andrew.henson@foxsoft.net>
2008-05-12 11:35:13 +0000

    [ADDED] Extension to create a Foxsoft base library for extension development, holding functions that are generic and useful across extensions
    git-svn-id: http://dali/svn/repos/projects/HRB/hrb.ie/trunk/src/typo3conf/ext/foxbase@875 610f52c9-3012-0410-9153-c0401b402bb8

---
+++ class.fox_febase.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+* 
+*/
+require_once(PATH_tslib.'class.tslib_pibase.php');
+
+class fox_febase extends tslib_pibase
+{
+
+	function resultsBrowser() {
+		$wrapArr = array(
+			'browseBoxWrap' => '<div class="browseBoxWrap">|</div>',
+			'showResultsWrap' => '<div class="showResultsWrap">|</div>',
+			'browseLinksWrap' => '<div class="browseLinksWrap"><ul>|</ul></div>',
+			'showResultsNumbersWrap' => '<span class="showResultsNumbersWrap">|</span>',
+			'disabledLinkWrap' => '<li class="disabledLinkWrap">|</li>',
+			'inactiveLinkWrap' => '<li class="inactiveLinkWrap">|</li>',
+			'activeLinkWrap' => '<li class="activeLinkWrap">|</li>'
+		);
+		
+		$this->internal['dontLinkActivePage'] = 1;
+		$this->internal['pagefloat'] = "center";
+	    $this->internal['showFirstLast'] = 1;
+
+		// only show page links if there's enough results to warrant it
+		if ($this->internal['results_at_a_time']<$this->internal['res_count']) {
+			return $this->pi_list_browseresults(1,'',$wrapArr);
+		} else { // just display the number of results
+			return $this->pi_list_browseresults(2,'',$wrapArr);
+		}
+	}
+	
+	function noResults()
+	{
+		// Show No Results Template
+		$tCode = $this->cObj->getSubpart($this->templateFile,'###TEMPLATE_NO_RESULTS###');
+		
+		// Fill in any markers with content accordingly e.g.
+		// $noResMarkers = array('###TITLE###' => 'hello world');
+		$noResMarkers = array();
+		
+		return $this->cObj->substituteMarkerArrayCached($tCode, $noResMarkers);
+	}
+
+	
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/foxbase/class.fox_febase.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/foxbase/class.fox_febase.php']);
+}
+
+?>
---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "foxbase"
+#
+# Auto generated 12-05-2008 11:09
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Foxsoft Base Classes',
+	'description' => 'Classes used for base functions across extensions developed by Foxsoft',
+	'category' => 'misc',
+	'author' => 'Andrew Henson',
+	'author_email' => 'andrew.henson@foxsoft.net',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"7111";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"f373";s:20:"doc/wizard_form.html";s:4:"1613";}',
+);
+
+?>
